ab3927c2b000f8c71a0c77f759421b03c47db84c
Akhmad Daniel Sembiring <akhmad.daniel@gmail.com>
2017-08-02 14:43:16 +0700

    authors

--- vit_inv_receipt/__openerp__.py
+++ vit_inv_receipt/__openerp__.py
@@ -5,19 +5,23 @@
 		"base",
 		"account"
 	], 
-	"author": "akhmad.daniel@gmail.com", 
+	"author": "Akhmad D. Sembiring [vitraining.com]",
+	"website": "www.vitraining.com",
 	"category": "Accounting",
 	"description": """\
 
-Manage
+Features
 ======================================================================
 
-Print invoice receipts.
+* Print invoice receipts.
+* Addons ini adalah bahan praktek pada traning "Odoo QWeb Programming" yang diselenggarakan di vitraining.com dan E-Book "Panduang Lengkap Pemrograman QWeb Odoo v10"
+* Klik http://shop.vitraining.com untuk info lebih lanjut
 
 """,
 	"data": [
 		"report/invoice.xml",
 	],
 	"installable": True,
+	"application": True,
 	"auto_install": False,
 }
