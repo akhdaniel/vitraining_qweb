f41d6fc10614e6edf0e656ee00276044cbac745d
Akhmad Daniel Sembiring <akhmad.daniel@gmail.com>
2017-08-02 14:44:20 +0700

    so sign authors

--- vit_so_sign/__openerp__.py
+++ vit_so_sign/__openerp__.py
@@ -5,16 +5,21 @@
 		"base",
 		"sale"
 	], 
-	"author": "akhmad.daniel@gmail.com", 
+	"author": "Akhmad D. Sembiring [vitraining.com]",
+	"website": "www.vitraining.com",
 	"category": "Sale",
 	"description": """\
 
-Manage
+Features
 ======================================================================
-Add new tabel under SO template for signature:
-* Created By
-* Validated By
-* Received by
+
+* Modify the standard SO report
+* Add new tabel under SO template for signature:
+	* Created By
+	* Validated By
+	* Received by
+* Addons ini adalah bahan praktek pada traning "Odoo QWeb Programming" yang diselenggarakan di vitraining.com dan E-Book "Panduang Lengkap Pemrograman QWeb Odoo v10"
+* Klik http://shop.vitraining.com untuk info lebih lanjut
 
 
 """,
@@ -22,6 +27,7 @@ Add new tabel under SO template for signature:
 		"report/so.xml",
 		"view/so.xml",
 	],
+	"application": True,
 	"installable": True,
 	"auto_install": False,
 }
