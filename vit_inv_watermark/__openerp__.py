139b54df8fb731b1d8e005207cab283162d11307
Akhmad Daniel Sembiring <akhmad.daniel@gmail.com>
2017-08-03 10:04:57 +0700

    ignore, __openerp__.py

--- vit_inv_watermark/__openerp__.py
+++ vit_inv_watermark/__openerp__.py
@@ -1,20 +1,26 @@
 {
-	"name": "Add Watermark on Invoice",
+	"name": "Add Watermark Image on Invoice",
 	"version": "1.0", 
 	"depends": [
 		"base",
 		"account"
 	], 
-	"author": "akhmad.daniel@gmail.com", 
+	"author": "Akhmad D. Sembiring [vitraining.com]",
+	"website": "www.vitraining.com",
 	"category": "Accounting",
 	"description": """\
+Features
+======================================================================
 
-Add Watermark on Invoice
+* Add Watermark on Invoice depending it's state: Open, Draft, Paid
+* Addons ini adalah bahan praktek pada traning "Odoo QWeb Programming" yang diselenggarakan di vitraining.com dan E-Book "Panduang Lengkap Pemrograman QWeb Odoo v10"
+* Klik http://shop.vitraining.com untuk info lebih lanjut
 
 """,
 	"data": [
 		"report/invoice.xml",
 	],
 	"installable": True,
+	"application": True,
 	"auto_install": False,
 }
--- vit_journal_voucher/__openerp__.py
+++ vit_journal_voucher/__openerp__.py
@@ -5,19 +5,23 @@
 		"base",
 		"account"
 	], 
-	"author": "akhmad.daniel@gmail.com", 
+	"author": "Akhmad D. Sembiring [vitraining.com]",
+	"website": "www.vitraining.com",
 	"category": "Accounting",
 	"description": """\
 
-Manage
+Features
 ======================================================================
 
-Print account move journal voucher
+* Print account move journal voucher
+* Addons ini adalah bahan praktek pada traning "Odoo QWeb Programming" yang diselenggarakan di vitraining.com dan E-Book "Panduang Lengkap Pemrograman QWeb Odoo v10"
+* Klik http://shop.vitraining.com untuk info lebih lanjut
 
 """,
 	"data": [
 		"report/account_move.xml",
 	],
 	"installable": True,
+	"application": True,
 	"auto_install": False,
 }
